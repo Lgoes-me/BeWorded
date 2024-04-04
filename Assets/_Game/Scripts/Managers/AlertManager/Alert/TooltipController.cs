da7705d7b0943fe885a3eb11ebef56b00a65ac07
Góes <lucas.rodrigues.goes@gmail.com>
2024-04-04 16:49:54 -0300

    close on deselect

--- Assets/_Game/Scripts/Managers/AlertManager/Alert/TooltipController.cs
+++ Assets/_Game/Scripts/Managers/AlertManager/Alert/TooltipController.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Threading.Tasks;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TooltipController : MonoBehaviour
+public class TooltipController : MonoBehaviour, IDeselectHandler
 {
     [field: SerializeField] private RectTransform Content { get; set; }
     [field: SerializeField] private TextMeshProUGUI Text { get; set; }
@@ -14,6 +16,9 @@ public class TooltipController : MonoBehaviour
 
     private Canvas PointTo { get; set; }
     private TaskCompletionSource<bool> CompletionSource { get; set; }
+
+    private Coroutine Coroutine { get; set; }
+    private WaitForSeconds WaitForSeconds { get; set; }
 
     public Task Show(string text, RectTransform pointTo, bool needsConfirmation, float duration)
     {
@@ -26,8 +31,10 @@ public class TooltipController : MonoBehaviour
         }
         else
         {
+            SetSelected();
             ConfirmButton.gameObject.SetActive(false);
-            Invoke(nameof(Close), duration);
+            WaitForSeconds = new WaitForSeconds(duration);
+            Coroutine = StartCoroutine(CloseCoroutine());
         }
 
         if (pointTo != null)
@@ -35,7 +42,7 @@ public class TooltipController : MonoBehaviour
             PointTo = pointTo.AddComponent<Canvas>();
             PointTo.overrideSorting = true;
             PointTo.sortingOrder = 9;
-            
+
             if (!(pointTo.position.y <= Content.sizeDelta.y))
             {
                 UpArrow.gameObject.SetActive(true);
@@ -54,14 +61,41 @@ public class TooltipController : MonoBehaviour
         return CompletionSource.Task;
     }
 
+
+    private IEnumerator CloseCoroutine()
+    {
+        yield return WaitForSeconds;
+        Close();
+    }
+
     private void Close()
     {
         if (PointTo != null)
         {
             Destroy(PointTo);
         }
-        
+
+        if (Coroutine != null)
+        {
+            StopCoroutine(Coroutine);
+        }
+
         CompletionSource.SetResult(true);
         Destroy(this.gameObject);
+    }
+
+    private void SetSelected()
+    {
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        var selectedGameObject = ((PointerEventData) eventData).pointerCurrentRaycast.gameObject;
+        
+        if (selectedGameObject.transform.IsChildOf(transform))
+            return;
+
+        Close();
     }
 }
